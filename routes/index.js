aad9a08b3d23abb1bf6b50e107e7e6d3c3f4a3b4
NavreetK <navreet.sidhu1999@gmail.com>
2019-07-28 18:24:18 -0400

    erro solved

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+// get login page
+router.get('/login',(req,res,next)=>{
+  res.render('login',{});
+});
+
+// get about page
+router.get('/about',(req,res,next)=>{
+  res.render('about',{});
+});
+
+// get surveys page
+router.get('/surveys',(req,res,next)=>{
+  res.render('surveys',{});
+});
+
+// get contact page
+router.get('/contact',(req,res,next)=>{
+  res.render('contact',{});
+});
+
+// get services page
+router.get('/services',(req,res,next)=>{
+  res.render('services',{});
 });
 
 module.exports = router;
